d2dd8e8c384473e7dc0355a4f2807f0013f30dca
MikaelEngvall <mikael.engvall.me@gmail.com>
2024-12-03 08:09:51 +0100

    test

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 const express = require('express');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express'); // Add Swagger
+const swaggerDocument = require('./swagger.json'); // Adjust the path if needed
 
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController'); 
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// Middleware
 app.use(express.json());
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.static(`${__dirname}/public`));
 
+// Swagger Docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middleware to add request time
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// Mounting routes
+// Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
+// Handle unhandled routes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// Global Error Handler
 app.use(globalErrorHandler);
 
 module.exports = app;
