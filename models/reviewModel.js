f9b4756beb3713e57251e98ae48e6eec65f91095
MikaelEngvall <mikael.engvall.me@gmail.com>
2024-12-07 21:15:15 +0100

    completed swagger

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -38,11 +38,11 @@ const reviewSchema = new mongoose.Schema({
 );
 reviewSchema.pre(/^find/, function(next) {
     this.populate({
-      path: 'user',
-      select: 'name'
-    }).populate({
         path: 'tour',
         select: 'name'
+    }).populate({
+        path: 'user',
+        select: 'name photo'
     });
     next();
   });
