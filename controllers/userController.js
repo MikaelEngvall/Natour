b6636629774743734b13fbb6f378b282fb8c9b6c
MikaelEngvall <mikael.engvall.me@gmail.com>
2024-12-08 20:08:55 +0100

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,33 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(
+            new AppError(
+                'Cannot update password with this endpoint', 
+                400
+            )
+        );
+    }
+    const filteredBody = req.body.filterObj(req.body, 'name', 'email');
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
+
+    res.status(200).json({
+        status:'success',
+        data: { updatedUser }
+    });
+});
+
 exports.deleteMe = catchAsync(async (req, res, next) => {
     await User.findByIdAndUpdate(req.user.id, { active: false });
 
@@ -10,7 +37,6 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
         data: null
     });
 });
-
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
