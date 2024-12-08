eef3808d5bb5f2a85827c8b7abef711a4d39bb88
MikaelEngvall <mikael.engvall.me@gmail.com>
2024-12-08 20:47:47 +0100

    more security

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,8 @@
 const Review = require('./../models/reviewModel');
 const factory = require('./handlerFactory');
 
+
+
 exports.getAllReviews = factory.getAll(Review);
 exports.setTourUserIds = (req, res, next) => {
     if(!req.body.tour) req.body.tour = req.params.tourId;
