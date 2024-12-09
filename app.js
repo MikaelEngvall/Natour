aea01f7c97b0f5f6b50055f3553a2240ae88e5c2
MikaelEngvall <mikael.engvall.me@gmail.com>
2024-12-09 19:50:26 +0100

    block in pug

--- app.js
+++ app.js
@@ -64,12 +64,22 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.status(200).render('base', { 
     tour: 'The Forest Hiker',
-    user: 'John Doe',
+    user: 'Mikael',
     title: 'Natours' 
   } );
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', { 
+    title: 'All Tours' 
+  });
+})
 
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', { 
+    title: 'The Forest Hiker Tour' 
+  });
+})
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
